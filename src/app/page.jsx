39a0467ffa299b3309dff4c4923c723ada143fb8
Ace <aceonsteam001@gmail.com>
2023-06-12 14:55:50 +0800

    add: expertise section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,46 @@ import './app.css'
 import MainLayout from '@/components/layouts/main-layout'
 import Button from '@/components/button';
 
+import Image from 'next/image';
+
+import { expertise } from '../data/Expertise'
+
 export default function Home() {
   return (
     <MainLayout>
-      <div className="w-1/2 mt-4 home">
-        <h1 className='bold font-bold heading'>Forms Holistic Digital Approaches to Grow Online Presence.</h1>
-        <p className='text-xl my-4'>The Know How Tech is a leader in the digital realm, providing businesses with the tools to transition from traditional to digital operations. Our platform offers a comprehensive set of resources to help businesses increase their profitability.</p>
-        <Button>Get started</Button>
+      <div className='md:flex justify-between'>
+        <div className="w-full lg:w-1/2 mt-4 home">
+          <h1 className='bold font-bold heading'>Forms Holistic Digital Approaches to Grow Online Presence.</h1>
+          <p className='desc mt-2 mb-4'>The Know How Tech is a leader in the digital realm, providing businesses with the tools to transition from traditional to digital operations. Our platform offers a comprehensive set of resources to help businesses increase their profitability.</p>
+          <Button>Get started</Button>
+        </div>
+
+        <Image  
+          src="/assets/illustration/ai4.jpeg"
+          alt='Home hero'
+          width={500}
+          height={500}/>
       </div>
+
+      <div>
+        <h1 className='heading-2 font-bold text-secondary'>Our expertise</h1>
+
+        <div className='grid grid-cols-4 my-4 gap-8'>
+          {expertise.map((expertise_items) => (
+            <div className='flex flex-col'>
+              <Image  
+                src={expertise_items.icon}
+                alt='Expertise icon'
+                width={75}
+                height={75}/>
+
+                <h2 className='title font-bold text-secondary'>{expertise_items.title}</h2>
+                <p className='my-2'>{expertise_items.desc}</p>
+                <p className='font-bold hover:underline cursor-pointer hover:text-primary mt-auto'>Learn more &gt;</p>
+            </div>
+          ))}
+        </div>
+      </div>  
     </MainLayout>
   );
 }
---
+++ src/data/Expertise.jsx
@@ -0,0 +1,44 @@
+export const expertise = [
+  {
+    icon: "/assets/icons/expertise/protection.png",
+    title: "Client Online Security",
+    desc: "We have a team of experts available 24/7 to make sure that your business is safe and protected from phishing, cookie theft, denial of service attacks, and more.",
+    links: "/",
+  },
+  {
+    icon: "/assets/icons/expertise/online-support.png",
+    title: "Customer Assistance",
+    desc: "Know How Tech provides businesses with efficient solutions for launching websites. We offer top-notch client services, utilizing a range of customer service software tools to meet your specific requirements.",
+    links: "/",
+  },
+  {
+    icon: "/assets/icons/expertise/communicate.png",
+    title: "Digital Marketing and Retargeting",
+    desc: "Our strategy involves targeted advertising on relevant websites and apps to engage users based on their online behavior and interests, ultimately driving them back to your websites.",
+    links: "/",
+  },
+  {
+    icon: "/assets/icons/expertise/analysis.png",
+    title: "Enhancing Business Tactics and Strategies",
+    desc: "We provide a range of effective business tactics and strategies, using strategic planning software like Cascade, KPISOFT, Perdoo, etc., to support your success in the business world.",
+    links: "/",
+  },
+  {
+    icon: "/assets/icons/expertise/tech-education.png",
+    title: "Technological Drive and Education",
+    desc: "Our services include an in-house development team and an online course covering computer basics, WordPress, and mobile app development.",
+    links: "/",
+  },
+  {
+    icon: "/assets/icons/expertise/web-design.png",
+    title: "Website Construction and Design",
+    desc: "The Know How Tech provides website design and construction services using a skilled team of developers and designers proficient in Adobe XD, Webflow, Bubble, and other tools.",
+    links: "/",
+  },
+  {
+    icon: "/assets/icons/expertise/web-development.png",
+    title: "Website Implementation and Development",
+    desc: "Designing and building websites, hosting, CRM integration, web development, and more are just some of the many things we expertly offer.",
+    links: "/",
+  },
+]
