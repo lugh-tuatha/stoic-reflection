799bef1c1421ffc25a6cf2e91cc11798d5181179
Ace <aceonsteam001@gmail.com>
2023-07-01 12:21:13 +0800

    improve: contact styles

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import './form.css'
 
+import { contacts } from '../../data/Contacts'
+
 function ContactForm() {
   return (
-    <div>
-      <form className=' text-center mx-auto mt-12'>
-        <div className='grid grid-cols-2 gap-2'>
-          <input type="text" placeholder='First Name' className='forms-input'/>
-          <input type="text" placeholder='Last Name' className='forms-input'/>
-          <input type="text" placeholder='Company Name' className='forms-input'/>
-          <input type="email" placeholder='Email Address' className='forms-input'/>
-          <input type="number" placeholder='Phone Number' className='forms-input'/>
-          <input type="text" placeholder='Website URL' className='forms-input'/>
-        </div>
-        <textarea placeholder='Please provide a description of your need or project' className='form-textarea'></textarea>
+    <form className=' text-center mx-auto mt-4' noValidate>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
+        {contacts.map((contact_item, index) => (
+          <div key={contact_item.id} className={`relative inputBox
+                ${contact_item.required === true ? 'isRequired' : ''}
+              `}>
+            <input type={contact_item.type} className='forms-input w-full rounded-sm' required/>
+            <span className='absolute left-0 py-3 text-lg pl-4 pointer-events-none duration-500'>{contact_item.placeholder}</span>
+          </div>
+        ))}
+      </div>
+      <div className='relative inputBox mt-5'>
+        <textarea className='form-textarea' required/>
+        <span className='absolute left-0 py-3 text-lg pl-4 pointer-events-none duration-500'>Please provide a description of your need or project</span>
+      </div>
 
-        <input type="submit" className='button'/>
-      </form>
-    </div>
+      <input type="submit" className='button '/>
+    </form>
   )
 }
 
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -6,9 +6,9 @@ import ContactForm from '@/components/contact/ContactForm'
 function Contact() {
   return (
     <MainLayout>
-      <div className='text-center w-full md:w-3/4 mx-auto'>
+      <div className='text-center w-full lg:w-3/4 mx-auto'>
         <h1 className='heading'>Contact us</h1>
-        <p className='desc font-semibold'>If you’re looking for a winning digital resource combinations for your business, then it’s time to contact The Know How Tech. Our team is a cohesive, highly experienced persons that is here to help you know the difference about being traditional to modernized.</p>
+        <p className='desc font-semibold mt-4'>If you’re looking for a winning digital resource combinations for your business, then it’s time to contact The Know How Tech. Our team is a cohesive, highly experienced persons that is here to help you know the difference about being traditional to modernized.</p>
 
         <ContactForm />
       </div>
